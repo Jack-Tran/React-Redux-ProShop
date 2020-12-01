4c0f540ce33b7ff5fa215e5683a7d69e0cd3d4f5
Jack <tran0165@Algonquinlive.com>
2020-11-30 20:44:28 -0500

    E-commerce web app built with react and redux

---
+++ client/src/components/Product.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-10 p-2 rounded h-100">
+      <Link to={`/product/${product.id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product.id}`}>
+          <Card.Title as="div" style={{ textAlign: "center" }}>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="div" style={{ textAlign: "center" }} className="p-1">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+
+        <Card.Text as="h3" style={{ textAlign: "center" }} className="p-2">
+          ${product.price}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
