e9fa24758810c4b8d65b2ddca4e7b1469c6f72e6
Jack <tran0165@Algonquinlive.com>
2020-12-02 22:15:42 -0500

    updating productPage with quantity

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -74,6 +74,19 @@ const ProductPage = ({ match }) => {
                   <ListGroup.Item>
                     <Row>
                       <Col>Quantity</Col>
+                      <Col>
+                        <Form.Control
+                          as="select"
+                          value={quantity}
+                          onChange={(e) => setQuantity(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
                     </Row>
                   </ListGroup.Item>
                 )}
