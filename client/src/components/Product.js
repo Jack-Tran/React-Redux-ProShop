0273e6ab785d4a258852bc33f90a39d0dbdd2bb6
Jack <tran0165@Algonquinlive.com>
2020-12-02 22:05:02 -0500

    added single product action and reducer

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -6,12 +6,12 @@ import Rating from "./Rating";
 const Product = ({ product }) => {
   return (
     <Card className="my-10 p-2 rounded h-100">
-      <Link to={`/product/${product.id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </Link>
 
       <Card.Body>
-        <Link to={`/product/${product.id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div" style={{ textAlign: "center" }}>
             <strong>{product.name}</strong>
           </Card.Title>
