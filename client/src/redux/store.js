3b51f09aff39e3f35ec5bc35700672e316b1376c
Jack <tran0165@Algonquinlive.com>
2020-12-04 13:11:41 -0500

    added user actions, types and reducer for user login

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,13 +2,19 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers/root-reducer";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const cartItemFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
